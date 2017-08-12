a2d1eb2e28f560b01ce0029f0bd85147dded394a
Caleb Sacks <calebjs88@gmail.com>
2017-08-11 23:17:41 -0400

    create

--- matrix-stack.js
+++ matrix-stack.js
@@ -1,12 +1,31 @@
 /*
- * Represents a stack of 3x2 row-major matrices (that can be applied to a 2-long vector (x, y))
+ * @file Represents a stack of 3x2 row-major matrices that look like this:
+ *
  * |a b c|
  * |d f g|
+ *
+ * ... in object-oriented style.
+ *
+ * @author clabe45
  */
-function MatrixStack() {
+
+/**
+ * Creates a new MatrixStack instance. If context is provided, then it will be bound to the matrix stack, and all transformations done
+ * to the current matrix will be applied to this context. Otherwise, [applyToContext]{@link MatrixStack#applyToContext(m)} must be called
+ * manually to apply the current matrix to a context.
+ * @param {CanvasRenderingContext2D} [context]
+ * @class
+ */
+function MatrixStack(context) {
 	this.loadIdentity();
+  if (context) this.context = context;
 }
-MatrixStack.prototype.multMatrix = function(m) {
+/**
+ * Multiplies current matrix by m, and overwrites current matrix slot
+ * @param {Number[]} matrix
+ */
+MatrixStack.prototype.multMatrix = function(matrix) {
+  var m = matrix;
 	var c = this.current;
 	var r = new Array(6);	// result
   c.push(0, 0, 1);  // final row
@@ -22,49 +41,81 @@ MatrixStack.prototype.multMatrix = function(m) {
 	}
 	this.stack[this.stack.length-1] = r;	// set current matrix
   this.current = r;
+  if (this.context) this.applyToContext(this.context);  // apply to context automatically, if context bound
 };
-MatrixStack.prototype.pushMatrix = function(m) {
+/**
+ * Copies the current matrix, and pushes it onto the stack for transformation.
+ */
+MatrixStack.prototype.pushMatrix = function() {
 	var copy = this.current.slice();
 	this.stack.push(copy);	// copy current matrix and push it onto the stack
 	this.current = copy;
 };
+/**
+ * Removes the current matrix from the stack.
+ */
 MatrixStack.prototype.popMatrix = function() {
 	if (this.stack.length > 0) this.stack.pop();
 	this.current = this.stack[this.stack.length-1];
 };
+/**
+ * Clears the stack of all matrices and then pushes the identity matrix onto it
+ */
 MatrixStack.prototype.loadIdentity = function() {
 	var identity = [	// identity matrix
   	1, 0, 0,
     0, 1, 0
-//     0, 0, 1
   ];
   this.stack = [identity];
   this.current = identity;
 }
+/**
+ * Multiplies the current matrix by a new translation matrix, constructed with the given arguments
+ * @param {Number} x
+ * @param {Number} y
+ */
 MatrixStack.prototype.translate = function(x, y) {
 	var matrix = [	//translation matrix
 		1, 0, x,
 		0, 1, y
-// 		0, 0, 1
 	];
 	this.multMatrix(matrix);
 }
+/**
+ * Multiples the current matrix by a new scale matrix, constructed with the given arguments
+ * @param {Number} x
+ * @param {Number} y
+ */
 MatrixStack.prototype.scale = function(x, y) {
 	var matrix = [
 		x, 0, 0,
 		0, y, 0
-//     0, 0, 1
 	];
 	this.multMatrix(matrix);
 };
+/**
+ * Multiplies the current matrix by a new rotation matrix, constructed with the given argument
+ * @param {Number} r the rotation expressed in radians
+ */
 MatrixStack.prototype.rotate = function(r) {
 	var matrix = [
 		  Math.cos(r), Math.sin(r), 0,
 	     -Math.sin(r), Math.cos(r), 0
-//           0,           0,           1
 	];
   this.multMatrix(matrix);
 };
+/**
+ * Binds this matrix stack to a CanvasRenderingContext2D instances. If already bound, replaces currently-bound context. Now all
+ * transformations will be applied to this context.
+ * @param {CanvasRenderingContext2D} context the new context to be bound to
+ */
+MatrixStack.prototype.bindToContext = function(context) {
+  this.context = context;
+}
+/**
+ * If this matrix stack is not bound to a CanvasRenderingContext2D instance, calls setTransform on context with the current matrix
+ * @param {CanvasRenderingContext2D} context the context to apply the current matrix to
+ */
 MatrixStack.prototype.applyToContext = function(context) {
 	var c = this.current;
 	context.setTransform(c[0], c[3], c[1], c[4], c[2], c[5]);	// switch to column-major
